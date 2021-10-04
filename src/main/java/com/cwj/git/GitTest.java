633f4fae74ebbb66cc34fe9a8bf6e2f988668340
cwj <Layne@atguigu.com>
2021-10-04 11:23:46 +0800

    first commit

---
+++ src/main/java/com/cwj/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.cwj.git;
+
+/**
+ * @author cwj
+ * @date 4/10/2021 上午 11:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+    }
+}
